fcd9f3ad5b46b9b2db0bf055e3dc64fdb1f6c611
jaredshito <raul_debek@hotmail.com>
2017-10-02 21:21:35 -0500

    .

---
+++ login/src/app/app.firebase.configure.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG =  {
+    apiKey: "AIzaSyDXd2z2q1Iv3KmcprXBsjZWIE3Hm-1gLI8",
+    authDomain: "fir-auth-77fb3.firebaseapp.com",
+    databaseURL: "https://fir-auth-77fb3.firebaseio.com",
+    projectId: "fir-auth-77fb3",
+    storageBucket: "fir-auth-77fb3.appspot.com",
+    messagingSenderId: "629079096096"
+  };
